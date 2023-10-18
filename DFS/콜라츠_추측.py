750c9ec40d394fab577cab742a2c7d429ca55452
sockki <poohct327@gmail.com>
2023-10-18 11:26:22 +0900

    10월 18일

---
+++ DFS/콜라츠_추측.py
@@ -0,0 +1,15 @@
+def solution(num):
+    answer = 0
+    if num == 1:
+        return 0
+    while True:
+        if num % 2 == 0:
+            num = num // 2
+        else:
+            num = num * 3 + 1
+        answer += 1
+        
+        if answer >= 500:
+            return -1
+        elif num == 1:
+            return answer
